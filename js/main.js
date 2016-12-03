afae42203f1a53838c56b958f276aeb7bb86a0f1
JosueELP <josueenrique1999@gmail.com>
2016-12-03 16:12:34 -0600

    Descripción de cada Platillo Parte 2
    Se logro exitosamente las transiciones y la forma en que cada descripcion aparezca cuando el usuario de click en el nombre del platillo, falta acomodar un poco el css para que quede bien acomodados "IMPORTANTE REACOMODAR LA PAGINA PARA CADA PLATILLO PARA DISPOSITIVOS PEQUEÑOS" pero eso sera a detalles para el final

--- js/main.js
+++ js/main.js
@@ -4,6 +4,29 @@ $(document).ready(function () {
 		$("#responsive-nav ul").toggleClass("active")
 		$(this).toggleClass("glyphicon-menu-hamburger")
 	})
-
+	//Se llama la funcion para cada descripcion de platillo
+	toggleFood(0)
+	toggleFood(1)
+	toggleFood(2)
+	toggleFood(3)
+	toggleFood(4)
+	toggleFood(5)
+	toggleFood(6)
+	toggleFood(7)
 });
-	
+//Funcion para cada descripcion del platillo
+function toggleFood(id) {
+
+	$("#plate_"+id).on("click",function(){
+		$("#id_"+id).animate({
+			top: "10%"
+		}, "slow")
+		//$("#id_0").toggleClass("hidden")
+	})
+	$("#close_"+id).on("click",function(event){
+		$("#id_"+id).animate({
+			top: "100%"
+		}, "slow")
+		//$("#id_0").toggleClass("hidden")
+	})
+}
